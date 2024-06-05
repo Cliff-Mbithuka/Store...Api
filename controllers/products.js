9f169511fd3b052afcc8728ed126a15af9ea9f83
Clifford Musyoka <your_email@example.com>
2024-06-05 15:25:07 +0300

    Add skip Method

--- controllers/products.js
+++ controllers/products.js
@@ -1,31 +1,42 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const products = await Product.find({  }).sort('-name price')
+  const products = await Product.find({})
+    .sort("name")
+    .select("name price")
+    .limit(10)
+    .skip(5);
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-    const {featured, company, name, sort} = req.query
-const queryObject = {}
+  const { featured, company, name, sort, fields } = req.query;
+  const queryObject = {};
 
-if(featured){
-    queryObject.featured = featured === 'true' ? true : false
-}
-if(company){
-  queryObject.company = company
-}
-if(name){
-  queryObject.name = { $regex: name, $options: 'i'}
-}
-// console.log(queryObject);
-  let result = Product.find(queryObject)
-  if(sort){
-    const sortList = sort.split(',').join(' ');
-    result = result.sort(sortList)
-  }else{
-    result = result.sort('createAt')
+  if (featured) {
+    queryObject.featured = featured === "true" ? true : false;
   }
-  const products = await result
+  if (company) {
+    queryObject.company = company;
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: "i" };
+  }
+  // console.log(queryObject);
+  let result = Product.find(queryObject);
+  //sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createAt");
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
